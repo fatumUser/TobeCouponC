811b1d453efc532137abfab127dfcc54e6c1fe03
fatumUser <129615173+fatumUser@users.noreply.github.com>
2023-09-26 11:16:03 +0300

    8888

---
+++ js/main.js
@@ -0,0 +1,79 @@
+
+function ContentLoaded() {
+  document.getElementById("slider").classList.toggle("slider-show");
+}
+function ContentLoaded_StorePage() {
+  document.getElementById("catalog").classList.toggle("catalog-show");
+}
+function ContentLoaded_FaqPage() {
+  document.getElementById("catalog_faq").classList.toggle("catalog-show");
+}
+function ContentLoaded_StoreCouponPage() {
+  document.getElementById("catalog_stores-coupons").classList.toggle("catalog-show");
+}
+function ContentLoaded_BlogPage() {
+  document.getElementById("catalog_blog").classList.toggle("catalog-show");
+}
+function ContentLoaded_BlogArticlePage() {
+  document.getElementById("catalog_blog-article").classList.toggle("catalog-show");
+}
+
+document.addEventListener("DOMContentLoaded", ContentLoaded);
+document.addEventListener("DOMContentLoaded", ContentLoaded_StorePage);
+document.addEventListener("DOMContentLoaded", ContentLoaded_FaqPage);
+document.addEventListener("DOMContentLoaded", ContentLoaded_StoreCouponPage);
+document.addEventListener("DOMContentLoaded", ContentLoaded_BlogPage);
+document.addEventListener("DOMContentLoaded", ContentLoaded_BlogArticlePage);
+
+function dropBlock() {
+  document.getElementById("drop-block").classList.toggle("drop-block-show");
+  document.getElementById("text_special-offers").classList.toggle("text_special-offers-show");
+}
+function footerMenuCoupons() {
+  document.getElementById("footer-col__title-1").classList.toggle("footer-col__title-show");
+}
+function footerMenuSocial() {
+  document.getElementById("footer-col__title-2").classList.toggle("footer-col__title-show");
+}
+function imgPlusMinus() {
+  document.getElementById("accordeon-block__img-plus").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus").classList.toggle("accordeon-block__img-minus-show");
+  document.getElementById("faq-top").classList.toggle("faq-top-show");
+}
+function imgPlusMinus_2() {
+  document.getElementById("accordeon-block__img-plus_2").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_2").classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_3() {
+  document.getElementById("accordeon-block__img-plus_3").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_3").classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_4() {
+  document.getElementById("accordeon-block__img-plus_4").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_4").classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_5() {
+  document.getElementById("accordeon-block__img-plus_5").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_5").classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_6() {
+  document.getElementById("accordeon-block__img-plus_6").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_6").classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_7() {
+  document.getElementById("accordeon-block__img-plus_7").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_7").classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_8() {
+  document.getElementById("accordeon-block__img-plus_8").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_8").classList.toggle("accordeon-block__img-minus-show");
+}
+function imgPlusMinus_9() {
+  document.getElementById("accordeon-block__img-plus_9").classList.toggle("accordeon-block__img-plus-none");
+  document.getElementById("accordeon-block__img-minus_9").classList.toggle("accordeon-block__img-minus-show");
+}
+
+function AccordeonPadding() {
+  document.getElementById("faq-top").classList.toggle("faq-top-show");
+}
+
